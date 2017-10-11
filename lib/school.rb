5484da40c6afd2e0e36f384244e463a70d10fdae
Adam Fudge <coolfudge@gmail.com>
2017-10-11 22:01:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+attr_accessor :name
+attr_reader :roster
+
+ROSTER = {}
+
+  def initialize(name)
+    @name = name
+    @roster = ROSTER
+  end
+
+  def add_student(name, grade)
+    if ROSTER.has_key? grade
+      ROSTER[grade] << name
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << name
+    end
+  end
+
+  def grade(grade)
+    return ROSTER[grade]
+  end
+
+  def sort
+    #ROSTER.collect |grade|
+    #  grade.sort
+    #end
+    ROSTER
+  end
+end
