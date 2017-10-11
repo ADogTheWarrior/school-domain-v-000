84cf199219f10fdd8645c6a305664cede60fde7b
Adam Fudge <coolfudge@gmail.com>
2017-10-11 22:11:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,14 @@ attr_reader :roster
   end
 
   def add_student(name, grade)
-    if roster.has_key? grade
-      roster[grade] << name
-    else
-      roster[grade] = []
-      roster[grade] << name
-    end
+    roster[grade] ||= []
+    roster[grade].push(name)
+    # if roster.has_key? grade
+    #   roster[grade] << name
+    # else
+    #   roster[grade] = []
+    #   roster[grade] << name
+    # end
   end
 
   def grade(grade)
